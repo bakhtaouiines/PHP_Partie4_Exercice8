f4dced8c145fd495e73e503f8a8f09bb8b908628
Ines Bakhtaoui <inesbkht@gmail.com>
2021-06-10 18:20:09 +0200

    exercice 4.8 fait

---
+++ exercice4.8.php
@@ -0,0 +1,18 @@
+<?php
+    function numbers($a = 25, $b = 78, $c = 2) : int {
+        return $a + $b + $c;
+    }
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 4.8</title>
+</head>
+<body>
+    <h1>Exercice 8 Partie 4 : Les fonctions PHP</h1>
+        <p><?=numbers()?></p>
+</body>
+</html>
